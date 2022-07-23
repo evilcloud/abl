513bfd6c208bf6a496a3fd787b698455281a40f8
GK <kg@GKs-MacBook-Pro.local>
2022-07-23 22:34:06 +0200

    dealing with subprocesses

--- col.py
+++ col.py
@@ -1,4 +1,10 @@
 import abel
+import subprocess
+
+
+subprocess.call(['pip3', 'install', 'humanize'])
+subprocess.call(['pip3', 'install', 'deta'])
+subprocess.call(['pip3', 'install', 'redis'])
 
 
 def run(cluster):
