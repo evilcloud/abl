8664d322873e551d98318c3b6ffefb426606184d
GK <kg@GKs-MacBook-Pro.local>
2022-07-24 10:02:24 +0200

    interruption instead of integration on the first level

--- col.py
+++ col.py
@@ -1,3 +1,4 @@
+from ast import arg
 import abel
 import sys
 import subprocess
@@ -7,9 +8,15 @@ def run(wallet):
     subprocess.call(['pip3', 'install', 'deta'])
     subprocess.call(['pip3', 'install', 'humanize'])
     wallet = True if wallet == '-p' or wallet == '--primary' else False
+
     print(f"Col lanuching {'WALLET' if wallet else 'CLUSTER'}")
     ret = abel.run(wallet)
     if ret == "EMERGENCY":
         sys.exit(1)
     else:
         sys.exit(0)
+
+
+if __name__ == '__main__':
+    arg1 = sys.argv[1] if len(sys.argv) > 1 else None
+    run(arg1)
